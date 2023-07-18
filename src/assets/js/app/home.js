476e15ccdbc9953b906f28db24dd16a63e47b87b
abhijith <abhijith@pixelflames.com>
2023-07-18 17:40:29 +0530

    pages done

--- src/assets/js/app/home.js
+++ src/assets/js/app/home.js
@@ -17,6 +17,15 @@ $(document).ready(function () {
     <path id="arrow-right-short_2_" data-name="arrow-right-short (2)" d="M4,21.74a2.463,2.463,0,0,1,2.463-2.463H34.994L24.42,8.709a2.466,2.466,0,1,1,3.487-3.487L42.682,20a2.463,2.463,0,0,1,0,3.487L27.906,38.259a2.466,2.466,0,1,1-3.487-3.487L34.994,24.2H6.463A2.463,2.463,0,0,1,4,21.74Z" transform="translate(-4 -4.5)" fill-rule="evenodd"/>
   </svg>  
   </button>`,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          useTransform: false,
+          speed: 500,
+        },
+      },
+    ],
   });
   $(".scroll_slider.forward").slick({
     speed: 5000,
@@ -34,7 +43,7 @@ $(document).ready(function () {
     variableWidth: true,
   });
   $(".scroll_slider.reverse").slick({
-    speed: 8000,
+    speed: 5000,
     infinite: true,
     autoplay: true,
     autoplaySpeed: 0,
@@ -47,10 +56,86 @@ $(document).ready(function () {
     touchMove: false,
     variableWidth: true,
     draggable: false,
-    rtl:true
+    rtl: true,
   });
-  Fancybox.bind("[data-fancybox]", {
-    // Your custom options
+  let fb = Fancybox.bind("[data-fancybox]", {
+    backdropClick: 0,
   });
-  $('.our_works_section .container_wrapper').css({'padding-bottom':`${($('.our_works_section .our_work_scroller .our_wok_slider_innerwrap .work_card').length * 300)+'px'}`})
+  $('[title="Close"]').click(function (e) {
+    e.preventDefault();
+    fb.close();
+  });
+
+  $("#hamburgermenu").click(function () {
+    if ($("body").hasClass("open_overlay")) {
+      $("body").removeClass("show_nav");
+      setTimeout(() => {
+        $("body").removeClass("open_overlay");
+      }, 800);
+    } else {
+      $("body").addClass("open_overlay");
+      setTimeout(() => {
+        $("body").addClass("show_nav");
+      }, 500);
+    }
+  });
+  if (window.screen.width < 580) {
+    $("header .header_inner_wrap nav ul li a").click(function () {
+      $("body").removeClass("show_nav");
+      setTimeout(() => {
+        $("body").removeClass("open_overlay");
+      }, 800);
+    });
+  }
+  if (window.screen.width < 1200) {
+    $("#portfolio_slider").slick({
+      speed: 500,
+      autoplay: false,
+      // autoplaySpeed: 100,
+      // cssEase: "linear",
+      infinite: false,
+      centerMode: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      prevArrow: `  <button class="slick_portfolio_arrows slick_left">
+      <svg xmlns="http://www.w3.org/2000/svg" width="39.405" height="34.482" viewBox="0 0 39.405 34.482">
+        <path id="arrow-right-short_2_" data-name="arrow-right-short (2)" d="M43.405,21.74a2.463,2.463,0,0,0-2.463-2.463H12.412L22.986,8.709A2.466,2.466,0,1,0,19.5,5.222L4.723,20a2.463,2.463,0,0,0,0,3.487L19.5,38.259a2.466,2.466,0,1,0,3.487-3.487L12.412,24.2H40.943A2.463,2.463,0,0,0,43.405,21.74Z" transform="translate(-4 -4.499)" fill-rule="evenodd"/>
+      </svg>
+    </button>`,
+      nextArrow: `  <button class="slick_portfolio_arrows slick_right"><svg xmlns="http://www.w3.org/2000/svg" width="39.405" height="34.482" viewBox="0 0 39.405 34.482">
+      <path id="arrow-right-short_2_" data-name="arrow-right-short (2)" d="M4,21.74a2.463,2.463,0,0,1,2.463-2.463H34.994L24.42,8.709a2.466,2.466,0,1,1,3.487-3.487L42.682,20a2.463,2.463,0,0,1,0,3.487L27.906,38.259a2.466,2.466,0,1,1-3.487-3.487L34.994,24.2H6.463A2.463,2.463,0,0,1,4,21.74Z" transform="translate(-4 -4.5)" fill-rule="evenodd"/>
+    </svg>  
+    </button>`,
+      useTransform: false,
+    });
+  }
+  if (window.screen.width > 1200) {
+    $(".our_works_section .container_wrapper").css({
+      "padding-bottom": `${
+        $(
+          ".our_works_section .our_work_scroller .our_wok_slider_innerwrap .work_card"
+        ).length *
+          300 +
+        "px"
+      }`,
+    });
+  }
+
+  var count = 0;
+  var innterval = setInterval(function () {
+    if (count == 100) {
+      clearInterval(innterval);
+      setTimeout(() => {
+        // $(".preloader_blade").fadeOut(700);
+        $("#loadingcount").fadeOut(200);
+        setTimeout(() => {
+          $(".preloader_blade").css({ transform: "translateY(-100%)" });
+        }, 300);
+      }, 500);
+    }
+    $("#loadingcount").html((count < 10 ? "0" : null) + count + "%");
+    // $(".progress_bar span").css({ width: count + "%" });
+    count++;
+  }, 30);
 });
--- src/assets/js/app/scroller.js
+++ src/assets/js/app/scroller.js
@@ -1,156 +1,218 @@
 gsap.registerPlugin(ScrollTrigger);
-
-if (window.screen.width > 1200) {
-  const locoScroll = new LocomotiveScroll({
-    el: document.querySelector("[data-scroll-container]"),
-    smooth: true,
-    lerp: 0.1,
-  });
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-  locoScroll.on("scroll", ScrollTrigger.update);
-  locoScroll.on(
-    "scroll",
-    (func = (scroll) => {
-      if (scroll.scroll.y > window.screen.height) {
-        $("body").addClass("stickyheader");
-      } else {
-        $("body").removeClass("stickyheader");
-      }
-    })
-  );
-
-  // tell ScrollTrigger to use these proxy methods for the "[data-scroll-container]" element since Locomotive Scroll is hijacking things
-  ScrollTrigger.scrollerProxy("[data-scroll-container]", {
-    scrollTop(value) {
-      return arguments.length
-        ? locoScroll.scrollTo(value, 0, 0)
-        : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector("[data-scroll-container]").style.transform
-      ? "transform"
-      : "fixed",
-  });
-
+$(document).ready(function () {
   if (window.screen.width > 1200) {
-    $(window).resize(function () {
-      locoScroll.update();
+    const locoScroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]"),
+      smooth: true,
+      lerp: 0.06,
+      // scrollbarContainer:$('main')
+    });
+    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+    locoScroll.on("scroll", ScrollTrigger.update);
+    locoScroll.on(
+      "scroll",
+      (func = (scroll) => {
+        // if (scroll.scroll.y > window.screen.height) {
+        if (scroll.scroll.y > 20) {
+          $("body").addClass("stickyheader");
+        } else {
+          $("body").removeClass("stickyheader");
+        }
+      })
+    );
+    $("header .header_inner_wrap nav ul li a[scroll-to]").click(function (e) {
+      e.preventDefault();
+      let href = $(this).attr("href");
+      let target = document.querySelector(`${href}`);
+      if (target) {
+        locoScroll.scrollTo(target);
+      }
+    });
+    // tell ScrollTrigger to use these proxy methods for the "[data-scroll-container]" element since Locomotive Scroll is hijacking things
+    ScrollTrigger.scrollerProxy("[data-scroll-container]", {
+      scrollTop(value) {
+        return arguments.length
+          ? locoScroll.scrollTo(value, 0, 0)
+          : locoScroll.scroll.instance.scroll.y;
+      }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        };
+      },
+      // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+      pinType: document.querySelector("[data-scroll-container]").style.transform
+        ? "transform"
+        : "fixed",
+    });
+
+    if (window.screen.width > 1200) {
+      $(window).resize(function () {
+        locoScroll.update();
+      });
+    }
+    if ($(".full_width_banner .inner_wrap").length > 0) {
+      gsap.from(".full_width_banner .inner_wrap", {
+        backgroundSize: "200%",
+        scrollTrigger: {
+          trigger: ".full_width_banner .inner_wrap",
+          start: "0% 100%",
+          end: "100% 0%",
+          scroller: "[data-scroll-container]",
+          scrub: true,
+          // pin:true,
+        },
+      });
+    }
+    if (
+      $(".about_intro .introduction .about_featured_image figure img").length >
+      0
+    ) {
+      gsap.to(".about_intro .introduction .about_featured_image figure img", {
+        scale: 1,
+        scrollTrigger: {
+          trigger:
+            ".about_intro .introduction .about_featured_image figure img",
+          start: "0% 100%",
+          end: "100% 0%",
+          scroller: "[data-scroll-container]",
+          scrub: true,
+          // pin:true,
+        },
+      });
+    }
+    if ($(".service_card figure img").length > 0) {
+      gsap.to(".service_card figure img", {
+        scale: 1,
+        scrollTrigger: {
+          trigger: ".service_card figure img",
+          start: "0% 100%",
+          end: "50% 0%",
+          scroller: "[data-scroll-container]",
+          scrub: true,
+          // pin:true,
+        },
+      });
+    }
+    if (
+      $(".hero_banner .banner_inner_wrap h1 .transform_wrap span").length > 0
+    ) {
+      gsap.to(".hero_banner .banner_inner_wrap h1 .transform_wrap span", {
+        y: -300,
+        scrollTrigger: {
+          trigger: ".hero_banner",
+          start: "100% 97%",
+          end: "100% -300px",
+          scroller: "[data-scroll-container]",
+          scrub: true,
+        },
+      });
+    }
+    let cardsLength = $(
+        ".our_works_section .our_work_scroller .our_wok_slider_innerwrap .work_card"
+      ).length,
+      multiplier = 300;
+    let totalSCroll = cardsLength * multiplier;
+    if (
+      $(".our_works_section .our_work_scroller .our_wok_slider_innerwrap")
+        .length > 0
+    ) {
+      gsap.to(
+        ".our_works_section .our_work_scroller .our_wok_slider_innerwrap",
+        {
+          x: -totalSCroll,
+          scrollTrigger: {
+            trigger:
+              ".our_works_section .our_work_scroller .our_wok_slider_innerwrap",
+            start: "50% 50%",
+            // ease: Bounce.easeOut,
+            end: `+=${totalSCroll}`,
+            scroller: "[data-scroll-container]",
+            scrub: true,
+            // pin:true,
+          },
+        }
+      );
+    }
+
+    $('form input[type="submit"]').click(function () {
+      setTimeout(() => {
+        locoScroll.update();
+      }, 1500);
+    });
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+  }
+  if ($(".hero_banner .banner_inner_wrap h1 .transform_wrap").length > 0) {
+    gsap.to(".hero_banner .banner_inner_wrap h1 .transform_wrap ", {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      stagger: 0.2,
+      delay: 4,
+      scrollTrigger: {
+        trigger: ".hero_banner .banner_inner_wrap h1 .transform_wrap ",
+      },
     });
   }
-  $(".about_intro .about_us_block .about_description p").each(function () {
-    gsap.to($(this), {
-      backgroundSize: "100% 100%",
-      scrollTrigger: {
-        trigger: $(this),
-        start: "0% 80%",
-        end: "100% 60%",
-        scroller: "[data-scroll-container]",
-        scrub: true,
-      },
+  if ($(".hero_banner .banner_inner_wrap").length > 0) {
+    gsap.to(
+      ".hero_banner .banner_inner_wrap .banner_desc .banner_desc_wrap h3",
+      {
+        y: "0%",
+        opacity: 1,
+        delay: 5.2,
+        // delay: 1.2,
+        // stagger:.3,
+        scrollTrigger: {
+          trigger:
+            ".hero_banner .banner_inner_wrap .banner_desc .banner_desc_wrap h3 ",
+        },
+      }
+    );
+    gsap.to(
+      ".hero_banner .banner_inner_wrap .banner_desc .banner_desc_wrap .cta_btn",
+      {
+        y: "0%",
+        opacity: 1,
+        delay: 5.5,
+        // stagger:.3,
+        scrollTrigger: {
+          trigger:
+            ".hero_banner .banner_inner_wrap .banner_desc .banner_desc_wrap .cta_btn ",
+        },
+      }
+    );
+  }
+  if ($(".about_intro").length > 0) {
+    $(".about_intro .about_us_block .about_description p").each(function () {
+      gsap.to($(this), {
+        backgroundSize: "100% 100%",
+        scrollTrigger: {
+          trigger: $(this),
+          start: "0% 80%",
+          end: "100% 60%",
+          scroller:
+            window.screen.width > 1200
+              ? "[data-scroll-container]"
+              : document.body,
+          scrub: true,
+        },
+      });
     });
-  });
-  gsap.from(".full_width_banner .inner_wrap", {
-    backgroundSize: "200%",
-    scrollTrigger: {
-      trigger: ".full_width_banner .inner_wrap",
-      start: "0% 100%",
-      end: "100% 0%",
-      scroller: "[data-scroll-container]",
-      scrub: true,
-      // pin:true,
-    },
-  });
-  gsap.to(".about_intro .introduction .about_featured_image figure img", {
-    scale: 1,
-    scrollTrigger: {
-      trigger: ".about_intro .introduction .about_featured_image figure img",
-      start: "0% 100%",
-      end: "100% 0%",
-      scroller: "[data-scroll-container]",
-      scrub: true,
-      // pin:true,
-    },
-  });
-  gsap.to(".service_card figure img", {
-    scale: 1,
-    scrollTrigger: {
-      trigger: ".service_card figure img",
-      start: "0% 100%",
-      end: "50% 0%",
-      scroller: "[data-scroll-container]",
-      scrub: true,
-      // pin:true,
-    },
-  });
-  let cardsLength = $(
-      ".our_works_section .our_work_scroller .our_wok_slider_innerwrap .work_card"
-    ).length,
-    multiplier = 300;
-  let totalSCroll = cardsLength * multiplier;
-
-  gsap.to(".our_works_section .our_work_scroller .our_wok_slider_innerwrap", {
-    x: -totalSCroll,
-    scrollTrigger: {
-      trigger:
-        ".our_works_section .our_work_scroller .our_wok_slider_innerwrap",
-      start: "50% 50%",
-      // ease: Bounce.easeOut,
-      end: `+=${totalSCroll}`,
-      scroller: "[data-scroll-container]",
-      scrub: true,
-      // pin:true,
-    },
-  });
-  gsap.to(".hero_banner .banner_inner_wrap h1 .transform_wrap span", {
-    y: "0%",
-    duration: 1,
-    stagger: 0.2,
-    scrollTrigger: {
-      trigger: ".hero_banner .banner_inner_wrap h1 .transform_wrap span",
-    },
-  });
-
-  gsap.to(".hero_banner .banner_inner_wrap .banner_desc .banner_desc_wrap h3", {
-    y: "0%",
-    opacity: 1,
-    delay: 1.2,
-    // stagger:.3,
-    scrollTrigger: {
-      trigger:
-        ".hero_banner .banner_inner_wrap .banner_desc .banner_desc_wrap h3 ",
-    },
-  });
-  gsap.to(
-    ".hero_banner .banner_inner_wrap .banner_desc .banner_desc_wrap .cta_btn",
-    {
-      y: "0%",
-      opacity: 1,
-      delay: 1.5,
-      // stagger:.3,
-      scrollTrigger: {
-        trigger:
-          ".hero_banner .banner_inner_wrap .banner_desc .banner_desc_wrap .cta_btn ",
-      },
+  }
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      document.querySelector("body").classList.add("stickyheader");
+    } else {
+      document.querySelector("body").classList.remove("stickyheader");
     }
-  );
-
-  $('form input[type="submit"]').click(function () {
-    setTimeout(() => {
-      locoScroll.update();
-    }, 1500);
   });
-
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-  ScrollTrigger.refresh();
-}
+});
